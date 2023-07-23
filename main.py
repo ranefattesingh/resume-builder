00b8f9992c76a42e59a8c75b87f2c48b93d1da99
Fattesingh Rane <ranefattesingh@gmail.com>
2023-07-23 15:20:27 +0530

    feat: add type to certain args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 from builders.resume_builder import ResumeBuilder
 from document.build import create_with_default_template
@@ -8,9 +9,13 @@ from decoding.decode_json import decode_json
 
 
 if __name__ == "__main__":
+    dynamic_files = sys.argv[1:]
     files = os.listdir(INPUT_DIR_NAME)
+   
     for file_name in files:
         json = decode_json(INPUT_DIR_NAME, file_name)
         resume = ResumeBuilder(json).add_header().add_educations().add_internships().add_work_experience().add_projects().add_skillset().add_achievements().add_interests().add_hobbies().add_languages().build()
 
         create_with_default_template(resume)
+
+    print("Task completed!")
