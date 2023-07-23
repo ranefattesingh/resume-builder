15c52d3f82047b8a27c6047b8497eab007d45a78
Fattesingh Rane <ranefattesingh@gmail.com>
2023-07-23 14:45:03 +0530

    feat: add test logs

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from decoding.decode_json import decode_json
 if __name__ == "__main__":
     files = os.listdir(INPUT_DIR_NAME)
     json = decode_json(INPUT_DIR_NAME, files[1])
-    print(json)
     resume = ResumeBuilder(json).add_header().add_educations().add_internships().add_work_experience().add_projects().add_skillset().add_achievements().add_interests().add_hobbies().add_languages().build()
 
     create_with_default_template(resume)
